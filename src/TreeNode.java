72dae436f3d1e0b340307f60f6ed9cffa2c623c9
YGZ <errrrorer@foxmail.com>
2016-04-25 10:48:20 +0800

    update

---
+++ src/TreeNode.java
@@ -0,0 +1,10 @@
+/**
+ * Created by YGZ on 2016/4/25.
+ * My email : errrrorer@foxmail.com
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
