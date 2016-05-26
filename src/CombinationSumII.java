38f8e69cad3de3358e3a2103c10528f878411684
YGZ <errrrorer@foxmail.com>
2016-05-26 09:25:19 +0800

    add

--- src/CombinationSumII.java
+++ src/CombinationSumII.java
@@ -12,10 +12,7 @@ public class CombinationSumII {
         if (candidates==null||candidates.length==0) return result;
         ArrayList<Integer> cur = new ArrayList<Integer>();
         Arrays.sort(candidates);
-        for (int i=0;i<candidates.length;i++) {
-            combine(candidates, target, i, cur, result);
-            if (i<candidates.length-1&&candidates[i] == candidates[i+1]) i+=1;
-        }
+        combine(candidates, target, 0, cur, result);
         return result;
     }
     public static void combine(int[] candidates,int target,int j,ArrayList<Integer> cur,List<List<Integer>> result){
@@ -29,13 +26,14 @@ public class CombinationSumII {
             cur.add(candidates[i]);
             combine(candidates,target-candidates[i],i+1,cur,result);
             cur.remove(cur.size()-1);
-            if (cur.isEmpty()) return;
+            while (i<candidates.length-1&&candidates[i] == candidates[i+1]) i+=1;
+            //if (cur.isEmpty()) return;
         }
     }
     public static void main(String args[]){
-        int[] nums = {5,3,3,4,5,2};
+        int[] nums = {1,1,1,1};
         CombinationSumII combinationSumII = new CombinationSumII();
-        List<List<Integer>> result = combinationSumII.combinationSum2(nums, 8);
+        List<List<Integer>> result = combinationSumII.combinationSum2(nums, 2);
         System.out.println(result);
     }
 }
